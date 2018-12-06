082a9fcdd4a70333723dc3fade42c4c54cd5b15d
danielurencio <danielurencio@gmail.com>
2018-12-06 03:54:01 -0600

    d

--- clipChallenge/script.js
+++ clipChallenge/script.js
@@ -5,7 +5,22 @@ arr.forEach(function(d) {
 	      .css('font-weight',800);
 });
 
+$('#Clip-Challenge').css('font-size','45px');
+
+$('.prompt').html('');
+
 $('p,li').css('font-family','Lato')
       	.css('font-size','20px')
       	.css('font-weight','300')
-	.css('text-align','justify');
+	.css('text-align','justify')
+	.css('line-height','1.4');
+
+Array.prototype.slice.call(document.querySelectorAll('span'))
+	.filter(function(d) {
+		return $(d).text() == 'Loading BokehJS ...';
+	})
+	.forEach(function(d) {
+		$(d).parent().css('display','none');
+	});
+
+$('.container').css('box-shadow','none')
